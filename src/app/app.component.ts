ff0fc1c7601be82880388ec93b0270580ad3182c
may <e.sirurd10@gmail.com>
2017-06-10 11:31:06 +0800

    任務 06：套用事件繫結 (Event Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // tslint:disable-next-line:no-inferrable-types
   inputHint: string = 'What needs to be done?';
+  inputItem = '';
+
+  todos: any[] = [];
+
+  addTodos(input: string) {
+    console.log(input);
+    // if (input !== '') {
+    //   this.todos.push(input);
+    // }
+    this.todos = [...this.todos, input];
+    console.log(this.todos);
+  }
+
+  inputWord(input: string) {
+    this.inputItem = input;
+  }
 }
